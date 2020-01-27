6dd0d1a14abf4d7cac73eecdd4238c3c6790f8f4
hani <38565286+wulihani@users.noreply.github.com>
2020-01-28 01:35:14 +0800

    Update mux_2.v

--- mux_2.v
+++ mux_2.v
@@ -1,7 +1,21 @@
-‘timescale 1ns/ips
+'timescale 1ns/ips
 
 //二选一数据分配器
 module mux_2(
 input EN,
-input in0,
-input in1,
+  input [15:0]in0,
+  input [15:0]in1,
+input sel,
+
+  output reg[15:0] out);
+  aways@(sel or en or in1 or in2 )
+  begin
+    if(!en)out <=16'd0;
+    else
+      case（sel）
+       0:out <= in0;
+       1:out <= in1;
+       default:out <=16'd0; 
+      endcase
+  end
+endmodule
